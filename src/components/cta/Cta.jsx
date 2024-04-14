0ea5643584848a0a21a8eb8eee2a2327a33c0d14
unknown <psabarikrishnan@gmail.com>
2024-04-14 22:33:21 +0530

    cta done

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -3,8 +3,14 @@ import './cta.css';
 
 const Cta = () => {
   return (
-    <div>
-      <h1>cta</h1>
+    <div className='gpt3__cta'>
+      <div className='gpt3__cta-content'>
+        <p>Request early access to get started</p>
+        <h3>Register today and start exploring the millions of possibilities</h3>
+      </div>
+      <div className='gpt3__cta-btn'>
+        <button type='button'>Get started</button>
+      </div>
     </div>
   )
 }
